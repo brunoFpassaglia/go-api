3ab5a47e238a646e03a3b6f5263735bf096a1ba6
Bruno Passaglia <bruno.passaglia2013@gmail.com>
2023-11-05 10:45:05 -0300

    Add config inicial do app

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"api/src/config"
 	"api/src/router"
+	"fmt"
 )
 
 func main() {
+	config.LoadConfig()
+	fmt.Println(config.Port)
 	r := router.Gerar()
-	r.Run()
+	r.Run(fmt.Sprintf(":%d", config.Port))
 }
