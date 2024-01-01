c44a4dbe988c1646f3860399817947f3d59ad59b
Bruno Passaglia <bruno.passaglia2013@gmail.com>
2024-01-01 10:28:45 -0300

    add post model and migration

---
+++ src/models/post.go
@@ -0,0 +1,13 @@
+package models
+
+import "time"
+
+type Post struct {
+	Id         uint64    `json:"id,omitempty"`
+	Title      string    `json:"title,omitempty"`
+	Content    string    `json:"content,omitempty"`
+	AuthorId   uint64    `json:"author_id,omitempty"`
+	AuthorNick string    `json:"author_nick,omitempty"`
+	Likes      uint64    `json:"likes"`
+	CreatedAt  time.Time `json:"created_at,omitempty"`
+}
